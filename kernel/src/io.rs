a37588d35c1709ae422c6e0fec6a01ea6e7761ee
newo-2001 <o.elderbroek@gmail.com>
2026-09-22 20:50:01 +0200

    Support read / write modes on io ports

--- kernel/src/io.rs
+++ kernel/src/io.rs
@@ -1,4 +1,4 @@
-use core::arch::asm;
+use core::{arch::asm, marker::PhantomData};
 
 unsafe fn in_byte(port: u16) -> u8 {
     let mut value = 0;
@@ -25,20 +25,59 @@ unsafe fn out_byte(port: u16, value: u8) {
 }
 
 #[derive(Clone, Copy)]
-pub struct IoPort(u16);
+pub struct IoPort<M = ModeReadWrite> {
+    port: u16,
+    _mode: PhantomData<M>,
+}
 
-impl IoPort {
+impl<M> IoPort<M>
+where
+    M: Mode,
+{
     pub const fn new(port: u16) -> Self {
-        Self(port)
-    }
-
-    pub unsafe fn in_byte(self) -> u8 {
-        unsafe { in_byte(self.0) }
-    }
-
-    pub unsafe fn out_byte(self, value: u8) {
-        unsafe {
-            out_byte(self.0, value);
+        Self {
+            port,
+            _mode: PhantomData,
         }
     }
 }
+
+impl<Mode> IoPort<Mode>
+where
+    Mode: Read,
+{
+    pub unsafe fn in_byte(self) -> u8 {
+        unsafe { in_byte(self.port) }
+    }
+}
+
+impl<Mode> IoPort<Mode>
+where
+    Mode: Write,
+{
+    pub unsafe fn out_byte(self, value: u8) {
+        unsafe {
+            out_byte(self.port, value);
+        }
+    }
+}
+
+pub trait Mode {}
+pub trait Read {}
+pub trait Write {}
+
+#[derive(Clone, Copy)]
+pub struct ModeRead;
+impl Read for ModeRead {}
+impl Mode for ModeRead {}
+
+#[derive(Clone, Copy)]
+pub struct ModeWrite;
+impl Write for ModeWrite {}
+impl Mode for ModeWrite {}
+
+#[derive(Clone, Copy)]
+pub struct ModeReadWrite;
+impl Read for ModeReadWrite {}
+impl Write for ModeReadWrite {}
+impl Mode for ModeReadWrite {}
